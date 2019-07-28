d912a376949492f579834bc870d0ff94262beb45
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-07-28 23:03:14 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
-end
+    validates :title, presence: true, uniqueness: true
+    validates :released, inclusion: { in: [ true, false ] }
+    validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, :if => :released?
+    validates :artist_name, presence: true
+  end
